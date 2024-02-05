66ae91f036b9f2efe25ce4d391a63ac11709d119
Dalia Ahmed <da257@njit.edu>
2024-02-05 15:26:30 -0500

    update details

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   useEffect(() => {
     const getRides = async () => {
       let res = await Client.get('/rides')
+      console.log(res.data)
       setRides(res.data)
     }
     getRides();
@@ -27,7 +28,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/rides' element={<Rides rides={rides}/>} />
-          <Route path='/rides/show' element={<Details rides={rides}/>} />
+          <Route path='/rides/:id' element={<Details rides={rides}/>} />
         </Routes>
       </main>
       <footer>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,24 +1,42 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import Client from '../services/api';
 
-const Details = () => {
+const Details = (props) => {
 
-    let id = useParams();
+    let { id } = useParams();
 
     const [rides, setRides] = useState('');
 
-    useEffect(() => {
-        let selectedRide = rides.find((ride) => ride.id === id)
-        setRides(selectedRide)
-    }, [])
+    const selectedRide = props.rides.find(ride => ride._id === id)
 
-    return (
+    const [review, setReview] = useState({
+        rating: '5',
+        content: ''
+    })
+
+    // const handleChange = (e) => {
+    //     setReview({ ...review, [e.target.name]: e.target.value })
+    // }
+
+    const fetchRide = async () => {
+        let res = await Client.get(`/rides/${id}`)
+        setRides(res.data)
+    }
+
+    useEffect(() => {
+        // setRides(selectedRide)
+        fetchRide()
+    }, [id])
+
+    return rides ? (
         <div>
             <div className='ride'>
                 <h1>Ride Details</h1>
-                <h2>{rides.description}</h2>
-                <h3>{rides.image}</h3>
+                <h2>{rides.title}</h2>
+                <p>{rides.description}</p>
             </div>
-            <div className='reviews'>
+            {/* <div className='reviews-container'>
                 <h3>Reviews</h3>
                 <form>
                     <label htmlFor='rating'>Rating:</label>
@@ -30,12 +48,20 @@ const Details = () => {
                         <option value="1">1</option>
                     </select>
                     <br />
-                    <input type="text" name="content" id="content" />
+                    <input type="text" name="content" id="content" onChange={handleChange}/>
                     <button type='submit'>Post</button>
                 </form>
-            </div>
+
+                <div className='reviews'>
+                    {rides.reviews.map(review => (
+                        <div>
+                            <p>{review.content}</p>
+                        </div>
+                    ))}
+                </div> */}
+            {/* </div> */}
         </div>
-    )
+    ) : null
 }
 
 export default Details
