ef69e406242b0bf047f301a9e2b4a9ceb72fc7c9
kfblack <kalinblack21@gmail.com>
2024-02-05 10:47:52 -0500

    Home and Ride components

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL
 
+const baseURL = "placeholder"
+
 const Client = axios.create({baseURL, BASE_URL})
 
 export default Client
